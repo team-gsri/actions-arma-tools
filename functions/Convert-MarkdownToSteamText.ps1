4584c4ea2ca541b325b0aa22ade1cf5c97d5b85e
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2022-09-27 23:14:43 +0200

    fix whitespaces

--- functions/Convert-MarkdownToSteamText.ps1
+++ functions/Convert-MarkdownToSteamText.ps1
@@ -10,7 +10,5 @@ Process {
     $markdown = ($Content -Join "`n") | ConvertFrom-Markdown
     $dom = New-Object -Com 'HTMLFile'
     $dom.write([System.Text.Encoding]::Unicode.GetBytes($markdown.Html))
-
-    $dom.body.childNodes | & $PSScriptRoot/ConvertTo-SteamText.ps1
-
+    return $dom.body.childNodes | & $PSScriptRoot/ConvertTo-SteamText.ps1
 }
